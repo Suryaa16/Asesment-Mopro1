524fc984d307a3266231da8758ad6a485eb31374
Surya Akbar Hilmi Saputra <hilmisaputra.y3@gmail.com>
2026-04-02 14:48:19 +0700

    First Commit

---
+++ app/src/main/java/com/surya607062400013/asesmentmobpro1/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.surya607062400013.asesmentmobpro1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.surya607062400013.asesmentmobpro1.ui.theme.AsesmentMOBPRO1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AsesmentMOBPRO1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AsesmentMOBPRO1Theme {
+        Greeting("Android")
+    }
+}
